8a5c3cb13e9fcca3f35b14537dd1cff2da7bdc82
hee462 <a01080809887@gmail.com>
2023-05-22 16:40:08 +0900

    은행계정제

---
+++ MyUser(2023-05-22).sql
@@ -0,0 +1,34 @@
+--여기는 DBA 권한을 갖는 myUser 화면입니다
+
+-- table 생성하기
+CREATE TABLE tbl_buyer(
+
+    buId	VARCHAR2(5)	NOT NULL	PRIMARY KEY ,
+    buName	nVARCHAR2(20)	NOT NULL,	
+    buTel	VARCHAR2(10),		
+    buAddr	nVARCHAR2(125),		
+    buBirth	VARCHAR2(10),		
+    buJob	nVARCHAR2(20)		
+
+);
+
+INSERT INTO tbl_buyer (buId,bybName,byAddr,buBirth,buJab)
+VALUES('0001','이몽룡','010-0000-1111','서울특별시','1999-01-01','직장인');
+SELECT *FROM tbl_buyer;
+
+INSERT INTO tbl_buyer(buID,buName)
+VALUES('0002','성춘향');
+
+SELECT*FROM tbl_buyer;
+
+UPDATE tbl_buyer
+SET butel ='010-222-2222'
+WHERE buId ='0002';
+
+
+SELECT*FROM tbl_buyer;
+
+
+UPDATE tbl_buyer
+SET buaddr = '전라북도 남원시'
+WHERE buname = '성춘향';
